22fecdd084be3f27f0385487c31c67305dbb688d
Siming Yang <yangsiming1999@gmai.com>
2018-03-26 10:36:54 -0700

    updated files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 8080;
+
 var app = express();
 
 app.set('view engine', hbs)
@@ -49,6 +51,6 @@ app.get('/404', (request, response) => {
 	})
 })
 
-app.listen(8080, () => {
+app.listen(port, () => {
 	console.log('Server is up on the port 8080');
 });
